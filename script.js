4fdfff0fa3ff3288b210f21de7734e510dd3b53b
shibam17 <shibam.dipu@gmail.com>
2020-11-30 12:35:38 +0530

    added files

---
+++ script.js
@@ -0,0 +1,27 @@
+var acc = document.getElementsByClassName("accordion");
+
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  })
